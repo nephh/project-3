3b451d301fbd3985daad14b8e5b92a83457a3457
Owen <owenmccr@gmail.com>
2024-02-01 20:54:45 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="bg-zinc-800 h-screen">
+        <h1 className="font-bold text-zinc-300">The Greatest Website Ever</h1>
+        <p>By: Owen, Jared, and Dustin</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
