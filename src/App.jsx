9c393eb067393ba07e8f5dba56586068b7e02814
Owen <owenmccr@gmail.com>
2024-02-01 21:07:50 -0500

    prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
     <>
-      <div className="bg-zinc-800 h-screen">
-        <h1 className="font-bold text-zinc-300">The Greatest Website Ever</h1>
+      <div className="h-screen bg-zinc-800">
+        <h1 className="font-bold text-zinc-300">
+          The Greatest Website Ever
+        </h1>
         <p>By: Owen, Jared, and Dustin</p>
       </div>
     </>
