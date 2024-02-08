3b004a56dd51b057e91dbf6bc41dfab0a13a8cb5
Owen <owenmccr@gmail.com>
2024-02-08 16:52:10 -0500

    go to dash on login and some styling

--- client/src/components/DashboardCommunities.jsx
+++ client/src/components/DashboardCommunities.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function DashboardCommunities({ communities }) {
+  // These are not specific to the logged in user yet! 
+  //
+
   return (
     <div>
       <h1 className="mb-4 text-xl font-bold text-zinc-200">Your Communities</h1>
       <div className="mx-auto flex max-w-sm flex-col rounded-lg bg-zinc-900 px-4 py-6 shadow-md">
-        <ul>
+        <ul className="border-t border-zinc-400 border-opacity-25">
           {communities.map((community, index) => (
-            <li key={index} className="flex items-center">
+            <li key={index} className="flex items-center border-b py-4 border-zinc-400 border-opacity-25">
               <p>
                 <Link
                   to={`/community/${community.url}`}
@@ -20,6 +23,7 @@ export default function DashboardCommunities({ communities }) {
                   Users: {community.userCount}
                 </span>
               </p>
+              <div className="my-2 border-t border-zinc-400" />
             </li>
           ))}
         </ul>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import DashboardCommunities from "../components/DashboardCommunities";
 import DashboardList from "../components/DashboardList";
 import { QUERY_COMMUNITIES, QUERY_ENDEAVORS } from "../utils/queries";
+import Auth from "../utils/auth";
 
 export default function Dashboard() {
   const { loading, data } = useQuery(QUERY_ENDEAVORS);
@@ -14,11 +15,17 @@ export default function Dashboard() {
 
   const endeavors = data?.endeavors || [];
   const communities = communityData?.communities || [];
+  const { data: user } = Auth.getUserInfo();
 
   return (
-    <div className="mt-4 flex flex-row justify-evenly">
-      <DashboardList endeavors={endeavors} />
-      <DashboardCommunities communities={communities} />
+    <div className="flex flex-col items-center">
+      <h2 className="mt-4 text-5xl font-bold text-zinc-200">
+        Welcome {user.username}!
+      </h2>
+      <div className="mt-4 flex flex-row justify-evenly">
+        <DashboardList endeavors={endeavors} />
+        <DashboardCommunities communities={communities} />
+      </div>
     </div>
   );
 }
